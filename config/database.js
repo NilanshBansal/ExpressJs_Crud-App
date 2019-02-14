5299621e66ce11f507f4a99c263ed1cc567813d1
Nilansh Bansal <bansalnilansh@gmail.com>
2019-02-14 15:19:16 +0530

    minor changes mongolab

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
     // database:'mongodb://localhost:27017/articlesDb',
-    database:'mongodb://nilansh:Nilansh%401997@ds135305.mlab.com:35305/articlesdb',
-    secret:'yoursecret'
+    database:'mongodb://Nilansh:Nilansh%401997@ds135305.mlab.com:35305/articlesdb',
+    // secret:'yoursecret'
 }
